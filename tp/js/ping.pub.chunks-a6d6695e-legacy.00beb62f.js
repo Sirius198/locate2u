61b4e1f9f4b217cd565bd04d4253783901f18d98
Sirius198 <mtwbp101@gmail.com>
2022-12-06 19:53:27 +0200

    update dist files

---
+++ tp/js/ping.pub.chunks-a6d6695e-legacy.00beb62f.js
@@ -0,0 +1,8 @@
+"use strict";(self["webpackChunkmun_explorer"]=self["webpackChunkmun_explorer"]||[]).push([[4858],{7543:function(e,n,r){r.d(n,{GWp:function(){return l},kIL:function(){return t}});var u=r(99354),l=(0,u.D)("Blank",""),t=(0,u.D)("PersonFill",'<path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>');
+/*!
+ * BootstrapVue Icons, generated from Bootstrap Icons 1.2.1
+ *
+ * @link https://icons.getbootstrap.com/
+ * @license MIT
+ * https://github.com/twbs/icons/blob/master/LICENSE.md
+ */}}]);
--- tp/js/ping.pub.chunks-a6d6695e-legacy.5fc364a4.js
+++
@@ -1,8 +0,0 @@
-"use strict";(self["webpackChunkblu_explorer"]=self["webpackChunkblu_explorer"]||[]).push([[4858],{7543:function(e,n,l){l.d(n,{GWp:function(){return u},kIL:function(){return t}});var r=l(99354),u=(0,r.D)("Blank",""),t=(0,r.D)("PersonFill",'<path fill-rule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>');
-/*!
- * BootstrapVue Icons, generated from Bootstrap Icons 1.2.1
- *
- * @link https://icons.getbootstrap.com/
- * @license MIT
- * https://github.com/twbs/icons/blob/master/LICENSE.md
- */}}]);
